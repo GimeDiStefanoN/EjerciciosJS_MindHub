cd91cda4e2b2a3cfbed6732a198662be3aca21f2
Gimena Di Stefano Nicolina <gimedistefano@gmail.com>
2023-03-02 19:17:06 -0300

    Cargo consignas objets, array, bucles

---
+++ objetos_arrays/objets_arrays.js
@@ -0,0 +1,41 @@
+                                            // OBJETOS
+let mascota = {
+    nombre: 'Jade',
+    nacimiento: '26/12/2013',
+    especie: 'canino',
+    raza: 'Beagle',
+    alimento: 'barf',
+    vacunas: true,
+    kilos: 12,
+    tutor: 'Gimena Di Stefano',
+    castracion: true,
+    temperamento: 'docil'
+}
+
+// OPERACIONES CON OBJETOS
+
+console.warn('modificar');
+console.warn('agregar');
+console.warn('eliminar');
+
+// METODOS
+console.warn('NewObjet');
+console.warn('Keys');
+console.warn('Assign');
+console.warn('Entries');
+console.warn('Values');
+
+
+                                            // ARRAY
+
+let selecciones = ['Argentina', 'Francia', 'Croacia', 'Marruecos', 'Inglaterra','Portugal', 'Paises Bajos', 'Brasil', 'EEUU', 'Ghana', 'Australia', 'Polonia', 'Japon', 'España', 'Corea del Sur', 'Suiza'];
+
+console.warn('pop');
+console.warn('push');
+console.warn('shift');
+console.warn('unshift');
+console.warn('indexOf');
+console.warn('includes');
+console.warn('toString');
+console.warn('join');
+console.warn('concat');

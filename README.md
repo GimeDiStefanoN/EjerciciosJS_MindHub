b901f7fc628c39d0bebb9f48875f0a4aed04b8a1
Gimena Di Stefano Nicolina <gimedistefano@gmail.com>
2023-03-03 11:01:10 -0300

    actualizacion ejerc y readme

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# Ejercicios JS_MindHub
+<h1 align="center">  Ejercicios JS_MindHub</h1>
+
+<p align="center">
+   <img src="https://img.shields.io/badge/STATUS-EN%20DESAROLLO-green">
+</p>
 
 Ejercicios de práctica sobre Javascript correspondientes al modulo 2 del bootcamp de Front End de Mindhub
+
+
+
+![ejerc_JS_preview](./img.png)
--- objetos_arrays/objets_arrays.js
+++ objetos_arrays/objets_arrays.js
@@ -1,6 +1,6 @@
                                             // OBJETOS
-let mascota = {
-    nombre: 'Jade',
+let mascotas = {
+    nombreMascota: 'Jade',
     nacimiento: '26/12/2013',
     especie: 'canino',
     raza: 'Beagle',
@@ -12,19 +12,51 @@ let mascota = {
     temperamento: 'docil'
 }
 
-// OPERACIONES CON OBJETOS
+    // OPERACIONES CON OBJETOS
 
-console.warn('modificar');
-console.warn('agregar');
-console.warn('eliminar');
+console.warn('modificar'); // modifica una propiedad del objeto que le pasemos
+    mascotas.alimento = 'royal canin';
+    console.log(mascotas);
 
-// METODOS
-console.warn('NewObjet');
-console.warn('Keys');
-console.warn('Assign');
-console.warn('Entries');
-console.warn('Values');
+console.warn('agregar'); // agrega una propiedad al objeto que le pasemos
+    mascotas.cirugiasExtra = false;
+    console.log(mascotas);
 
+console.warn('eliminar');  // elimina una propiedad al objeto que le pasemos
+    delete mascotas.kilos
+    console.log(mascotas);
+
+    // METODOS
+
+console.warn('NewObjet'); // crea un nuevo objeto tal como lo definamos
+    let tutores = new Object(
+        {
+            nombre: '',
+            apellido:'',
+            dni: '',
+            telefono: '',
+        }
+    );
+    console.log(tutores);
+
+    //le voy asignar valores al nuevo objeto
+    tutores.nombre = 'Gimena';
+    tutores.apellido = 'Di Stefano';
+    tutores.dni = '33751772';
+    tutores.telefono = '3513172081';
+    console.log(tutores);
+console.warn('Keys');  // muestra las claves del objeto que le pasemos
+    console.log(Object.keys(mascotas));
+
+console.warn('Assign'); // crea un array combinando los objetos que le pasemos
+    const paciente = Object.assign(mascotas, tutores);
+    console.log(paciente);
+
+console.warn('Entries'); // devuelve un array que dentro tiene tantos array como (clave, valor) tenga el objeto que le pasemos
+    console.log(Object.entries(mascotas));
+
+console.warn('Values'); // muestra los valores del objeto que le pasemos
+    console.log(Object.values(mascotas));
 
                                             // ARRAY
 

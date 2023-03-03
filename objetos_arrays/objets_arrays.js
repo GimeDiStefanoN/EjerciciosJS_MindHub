5973226df5014e772fb239d346380d04a195a754
Gimena Di Stefano Nicolina <gimedistefano@gmail.com>
2023-03-03 13:24:56 -0300

    ejerc obj_array completo

--- objetos_arrays/objets_arrays.js
+++ objetos_arrays/objets_arrays.js
@@ -60,14 +60,40 @@ console.warn('Values'); // muestra los valores del objeto que le pasemos
 
                                             // ARRAY
 
-let selecciones = ['Argentina', 'Francia', 'Croacia', 'Marruecos', 'Inglaterra','Portugal', 'Paises Bajos', 'Brasil', 'EEUU', 'Ghana', 'Australia', 'Polonia', 'Japon', 'España', 'Corea del Sur', 'Suiza'];
+let seleccionesClasificadas = ['Argentina', 'Francia', 'Croacia', 'Marruecos', 'Inglaterra','Portugal', 'Paises Bajos', 'Brasil', 'EEUU', 'Ghana', 'Australia', 'Polonia', 'Japon', 'España', 'Corea del Sur', 'Suiza'];
+    console.log(seleccionesClasificadas);
 
-console.warn('pop');
-console.warn('push');
-console.warn('shift');
-console.warn('unshift');
-console.warn('indexOf');
-console.warn('includes');
-console.warn('toString');
-console.warn('join');
-console.warn('concat');
+console.warn('pop');  //elimina el ultimo elemento del array
+    seleccionesClasificadas.pop();
+    console.log(seleccionesClasificadas);
+
+console.warn('push'); //añade elementos al final del array
+    seleccionesClasificadas.push('Suiza','Italia');
+    console.log(seleccionesClasificadas);
+console.warn('shift'); //elimina el primer elemento del array
+    seleccionesClasificadas.shift();
+    console.log(seleccionesClasificadas);
+
+console.warn('unshift'); //añade elementos al inicio del array
+        seleccionesClasificadas.unshift('Argentina', 'Chile');
+        console.log(seleccionesClasificadas);
+
+console.warn('indexOf');  // el primer índice en el que se puede encontrar un elemento dado en el array, ó retorna -1 si el elemento no esta presente.
+    console.log(seleccionesClasificadas.indexOf('Portugal'));
+    console.log(seleccionesClasificadas.indexOf('Canada'));
+
+console.warn('includes'); //Devuelve un booleano dependiendo si el elemento que le pasemos está o no en el array
+    console.log(seleccionesClasificadas.includes('Bolivia'));
+    console.log(seleccionesClasificadas.includes('Brasil'));
+
+console.warn('toString'); //muestra una cadena de caracteres con los elementos del array que le pasamos separados por una coma
+    console.log(seleccionesClasificadas.toString());
+
+console.warn('join'); //muestra una cadena de caracteres con los elementos del array que le pasamos separados por el "separador" que le pasemos.
+console.log(seleccionesClasificadas.join('-'));
+console.log(seleccionesClasificadas.join('&'));
+console.log(seleccionesClasificadas.join('   '));
+console.warn('concat');  //muestra un nuevo array  donde une los 2 array informados
+    // let seleccionesEliminadas = ['Qatar', 'Canadá','Ecuador', 'Irán', 'Gales', 'Dinamarca', 'Túnez', 'México', 'Arabia Saudita', 'Bélgica', 'Alemania', 'Costa Rica', 'Uruguay', 'Ghana', 'Serbia', 'Camerún']; 
+    // let mundialQatar = seleccionesClasificadas.concat(seleccionesEliminadas);
+    // console.log(mundialQatar);

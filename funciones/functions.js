2e663a09c932b8a1ebc812ccca1408ccccde6cc1
Gimena Di Stefano Nicolina <gimedistefano@gmail.com>
2023-03-03 15:09:56 -0300

    avance functions

---
+++ funciones/functions.js
@@ -0,0 +1,145 @@
+//                  EJERCICIOS PLATAFORMA
+
+// 1. Cree una función que, cuando se llame, devuelva una cadena con los datos de su presentación, p. "Hola Mi nombre es Adrián, tengo 39 años y vivo en Buenos Aires..."..
+console.warn('Ej. 0.1');
+
+
+// 2. Cree una función que reciba como parámetro un precio y calcule el IVA (21%) de este valor.
+console.warn('Ej. 0.2');
+
+
+// 3. Cree una función que llame a una matriz de números y los sume.
+console.warn('Ej. 0.3');
+
+
+// 4. Cree una función que reciba una edad y devuelva una alerta, si la edad pasada a la función es >18 "Puede conducir ahora" si <18 "Todavía no puede conducir".
+console.warn('Ej. 0.4');
+
+
+
+//                     EJERCICIOS EXTRA
+
+
+// 1 - Crear un array con un mínimo de 10 elementos numéricos, un array con un mínimo de 10 
+// strings y un array con un mínimo de 10 objetos {edad, nombre, altura, peso, sexo}
+    console.warn('Ej. 1');
+    const CANTIDAD = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const DEPORTES = [ 'Futbol', 'Voley','Hockey','Softball','Natación','Rugby','Handball','Waterpolo','Atletismo','Basquet',];
+    const ALUMNOS = [ 
+        {
+            edad: 15,
+            nombre: 'Gabriela',
+            altura: 1.40,
+            peso: 50,
+            sexo: 'Femenino'
+        },
+        {
+            edad: 18,
+            nombre: 'Pepe',
+            altura: 1.70,
+            peso: 75,
+            sexo: 'Masculino'
+        },
+        {
+            edad: 16,
+            nombre: 'Sofia',
+            altura: 1.60,
+            peso: 55,
+            sexo: 'Femenino'
+        },
+        {
+            edad: 19,
+            nombre: 'Carolina',
+            altura: 1.70,
+            peso: 65,
+            sexo: 'Femenino'
+        },
+        {
+            edad: 34,
+            nombre: 'Gimena',
+            altura: 1.66,
+            peso: 70,
+            sexo: 'Femenino'
+        },
+        {
+            edad: 28,
+            nombre: 'Nahuel',
+            altura: 1.80,
+            peso: 78,
+            sexo: 'Masculino'
+        },
+        {
+            edad: 16,
+            nombre: 'Jose',
+            altura: 1.60,
+            peso: 60,
+            sexo: 'Masculino'
+        },
+        {
+            edad: 27,
+            nombre: 'Micaela',
+            altura: 1.64,
+            peso: 60,
+            sexo: 'Femenino'
+        },
+        {
+            edad: 36,
+            nombre: 'Marcos',
+            altura: 1.80,
+            peso: 76,
+            sexo: 'Masculino'
+        },
+        {
+            edad: 20,
+            nombre: 'Isaac',
+            altura: 1.56,
+            peso: 60,
+            sexo: 'Masculino'
+        }
+    ];
+    console.log(CANTIDAD);
+    console.log(DEPORTES);
+    console.log(ALUMNOS);
+// 2 - Crear una función que sume todos los números del array de números o concatene el array de 
+// strings.
+console.warn('Ej. 2');
+
+
+// 3 - Crear una función que muestre una propiedad especifica de todos los objetos del array de 
+// objetos.
+console.warn('Ej. 3');
+
+
+// 4- Crear una función que sume solo los números impares.
+console.warn('Ej. 4');
+
+
+// 5 - Crear una función que me muestre las palabras con más de 10 letras.
+console.warn('Ej. 5');
+
+
+// 6 - Crear una función que imprima las personas mayores de 30 años con una altura menor a 
+// 170cm.
+console.warn('Ej. 6');
+
+
+// 7 - Crear una función que imprima el número más chico de los números.
+console.warn('Ej. 7');
+
+
+// 8 - Crear una función que me imprima el número más grande de los números.
+console.warn('Ej. 8');
+
+
+// 9 - Crear una función que me imprima el primer número PAR.
+console.warn('Ej. 9');
+
+
+// 10 - Crear una función que me imprima el primer menor de 30 años con más de 1.70 metros.
+console.warn('Ej. 10');
+
+
+// 11 - Crear una función que me imprima el porcentaje de Hombres y Mujeres que hay en el array 
+// de Objetos
+console.warn('Ej. 11');
+
--- objetos_arrays/objets_arrays.js
+++ objetos_arrays/objets_arrays.js
@@ -15,29 +15,29 @@ let mascotas = {
     // OPERACIONES CON OBJETOS
 
 console.warn('modificar'); // modifica una propiedad del objeto que le pasemos
-    mascotas.alimento = 'royal canin';
-    console.log(mascotas);
+    // mascotas.alimento = 'royal canin';
+    // console.log(mascotas);
 
 console.warn('agregar'); // agrega una propiedad al objeto que le pasemos
-    mascotas.cirugiasExtra = false;
-    console.log(mascotas);
+    // mascotas.cirugiasExtra = false;
+    // console.log(mascotas);
 
 console.warn('eliminar');  // elimina una propiedad al objeto que le pasemos
-    delete mascotas.kilos
-    console.log(mascotas);
+    // delete mascotas.kilos
+    // console.log(mascotas);
 
     // METODOS
 
 console.warn('NewObjet'); // crea un nuevo objeto tal como lo definamos
-    let tutores = new Object(
-        {
-            nombre: '',
-            apellido:'',
-            dni: '',
-            telefono: '',
-        }
-    );
-    console.log(tutores);
+    // let tutores = new Object(
+    //     {
+    //         nombre: '',
+    //         apellido:'',
+    //         dni: '',
+    //         telefono: '',
+    //     }
+    // );
+    // console.log(tutores);
 
     //le voy asignar valores al nuevo objeto
     tutores.nombre = 'Gimena';
@@ -45,18 +45,19 @@ console.warn('NewObjet'); // crea un nuevo objeto tal como lo definamos
     tutores.dni = '33751772';
     tutores.telefono = '3513172081';
     console.log(tutores);
+
 console.warn('Keys');  // muestra las claves del objeto que le pasemos
-    console.log(Object.keys(mascotas));
+    // console.log(Object.keys(mascotas));
 
 console.warn('Assign'); // crea un array combinando los objetos que le pasemos
-    const paciente = Object.assign(mascotas, tutores);
-    console.log(paciente);
+    // const paciente = Object.assign(mascotas, tutores);
+    // console.log(paciente);
 
 console.warn('Entries'); // devuelve un array que dentro tiene tantos array como (clave, valor) tenga el objeto que le pasemos
-    console.log(Object.entries(mascotas));
+    // console.log(Object.entries(mascotas));
 
 console.warn('Values'); // muestra los valores del objeto que le pasemos
-    console.log(Object.values(mascotas));
+    // console.log(Object.values(mascotas));
 
                                             // ARRAY
 
@@ -64,35 +65,37 @@ let seleccionesClasificadas = ['Argentina', 'Francia', 'Croacia', 'Marruecos', '
     console.log(seleccionesClasificadas);
 
 console.warn('pop');  //elimina el ultimo elemento del array
-    seleccionesClasificadas.pop();
-    console.log(seleccionesClasificadas);
+    // seleccionesClasificadas.pop();
+    // console.log(seleccionesClasificadas);
 
 console.warn('push'); //añade elementos al final del array
-    seleccionesClasificadas.push('Suiza','Italia');
-    console.log(seleccionesClasificadas);
+    // seleccionesClasificadas.push('Suiza','Italia');
+    // console.log(seleccionesClasificadas);
+
 console.warn('shift'); //elimina el primer elemento del array
-    seleccionesClasificadas.shift();
-    console.log(seleccionesClasificadas);
+    // seleccionesClasificadas.shift();
+    // console.log(seleccionesClasificadas);
 
 console.warn('unshift'); //añade elementos al inicio del array
-        seleccionesClasificadas.unshift('Argentina', 'Chile');
-        console.log(seleccionesClasificadas);
+        // seleccionesClasificadas.unshift('Argentina', 'Chile');
+        // console.log(seleccionesClasificadas);
 
 console.warn('indexOf');  // el primer índice en el que se puede encontrar un elemento dado en el array, ó retorna -1 si el elemento no esta presente.
-    console.log(seleccionesClasificadas.indexOf('Portugal'));
-    console.log(seleccionesClasificadas.indexOf('Canada'));
+    // console.log(seleccionesClasificadas.indexOf('Portugal'));
+    // console.log(seleccionesClasificadas.indexOf('Canada'));
 
 console.warn('includes'); //Devuelve un booleano dependiendo si el elemento que le pasemos está o no en el array
-    console.log(seleccionesClasificadas.includes('Bolivia'));
-    console.log(seleccionesClasificadas.includes('Brasil'));
+    // console.log(seleccionesClasificadas.includes('Bolivia'));
+    // console.log(seleccionesClasificadas.includes('Brasil'));
 
 console.warn('toString'); //muestra una cadena de caracteres con los elementos del array que le pasamos separados por una coma
-    console.log(seleccionesClasificadas.toString());
+    // console.log(seleccionesClasificadas.toString());
 
 console.warn('join'); //muestra una cadena de caracteres con los elementos del array que le pasamos separados por el "separador" que le pasemos.
-console.log(seleccionesClasificadas.join('-'));
-console.log(seleccionesClasificadas.join('&'));
-console.log(seleccionesClasificadas.join('   '));
+    // console.log(seleccionesClasificadas.join('-'));
+    // console.log(seleccionesClasificadas.join('&'));
+    // console.log(seleccionesClasificadas.join('   '));
+
 console.warn('concat');  //muestra un nuevo array  donde une los 2 array informados
     // let seleccionesEliminadas = ['Qatar', 'Canadá','Ecuador', 'Irán', 'Gales', 'Dinamarca', 'Túnez', 'México', 'Arabia Saudita', 'Bélgica', 'Alemania', 'Costa Rica', 'Uruguay', 'Ghana', 'Serbia', 'Camerún']; 
     // let mundialQatar = seleccionesClasificadas.concat(seleccionesEliminadas);
